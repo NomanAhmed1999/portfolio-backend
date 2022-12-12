e7abdfa163047293f5e3119a55c6069773f144f9
NomanAhmed1999 <nomanahmed0919@gmail.com>
2022-12-12 23:55:24 +0500

    updated

--- controller/portfolio-data-controller.js
+++ controller/portfolio-data-controller.js
@@ -11,61 +11,61 @@ const all_portfolios = async (req, res) => {
 };
 
 // // Get Single Portfolio data
-const single_portfolio = async (req, res) => {
-    try {
-        const portfolioData = await portfolioDataSchema.findById(res.params.portfolioId);
-        res.json(portfolioData);
-    } catch (error) {
-        res.json({ message: error })
-    }
-};
+// const single_portfolio = async (req, res) => {
+//     try {
+//         const portfolioData = await portfolioDataSchema.findById(res.params.portfolioId);
+//         res.json(portfolioData);
+//     } catch (error) {
+//         res.json({ message: error })
+//     }
+// };
 
 
 
-// Add New Product
-const add_new_portfolio = async (req, res) => {
-    try {
-        const portfolioData = new portfolioDataSchema({
-            title: req.body.title,
-            price: req.body.pice
-        });
-        const savePortfolio = await portfolioDataSchema.save();
-        res.send(savePortfolio);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-};
+// // Add New Product
+// const add_new_portfolio = async (req, res) => {
+//     try {
+//         const portfolioData = new portfolioDataSchema({
+//             title: req.body.title,
+//             price: req.body.pice
+//         });
+//         const savePortfolio = await portfolioDataSchema.save();
+//         res.send(savePortfolio);
+//     } catch (error) {
+//         res.status(400).send(error);
+//     }
+// };
 
 
 
-// Update Portfolio data
-const update_portfolio = async (req, res) => {
-    try {
-        const portfolioData = {
-            title: req.body.title,
-            price: req.body.pice
-        };
-        const savePortfolio = await portfolioDataSchema.findByIdAndUpdate(
-            { _id: req.params.portfolioId },
-            portfolioData
-        );
-        res.send(savePortfolio);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-};
+// // Update Portfolio data
+// const update_portfolio = async (req, res) => {
+//     try {
+//         const portfolioData = {
+//             title: req.body.title,
+//             price: req.body.pice
+//         };
+//         const savePortfolio = await portfolioDataSchema.findByIdAndUpdate(
+//             { _id: req.params.portfolioId },
+//             portfolioData
+//         );
+//         res.send(savePortfolio);
+//     } catch (error) {
+//         res.status(400).send(error);
+//     }
+// };
 
 
 
-// Delete Portfolio data
-const delete_portfolio = async (req, res) => {
-    try {
-        const savePortfolio = await portfolioDataSchema.findByIdAndDelete(req.params.PortfolioId);
-        res.send(savePortfolio);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-};
+// // Delete Portfolio data
+// const delete_portfolio = async (req, res) => {
+//     try {
+//         const savePortfolio = await portfolioDataSchema.findByIdAndDelete(req.params.PortfolioId);
+//         res.send(savePortfolio);
+//     } catch (error) {
+//         res.status(400).send(error);
+//     }
+// };
 
 module.exports = {
     all_portfolios,
--- router/portfolio-data.js
+++ router/portfolio-data.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 const portfolioDataController = require("../controller/portfolio-data-controller");
 
 router.get("/", portfolioDataController.all_portfolios);
-router.post("/", portfolioDataController.add_new_portfolio);
-router.get("/:portfolioId", portfolioDataController.single_portfolio);
-router.put("/:portfolioId", portfolioDataController.update_portfolio);
-router.delete("/:portfolioId", portfolioDataController.delete_portfolio);
+// router.post("/", portfolioDataController.add_new_portfolio);
+// router.get("/:portfolioId", portfolioDataController.single_portfolio);
+// router.put("/:portfolioId", portfolioDataController.update_portfolio);
+// router.delete("/:portfolioId", portfolioDataController.delete_portfolio);
 
 
 module.exports = router;
